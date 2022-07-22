87b00eb9438cbd6aad8866edc89b570638416aca
goforGUSTO <jamesdoth@gmail.com>
2022-07-22 10:55:29 -0700

    link nav

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,18 @@
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
 const FirstPost = () => {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+            </h2>
+        </>
+    )
 }
 
 export default FirstPost
+
