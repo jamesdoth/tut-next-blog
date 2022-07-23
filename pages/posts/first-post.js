f0e6c4a2c7bf248b53af5160f4a466fcdb62b4e2
goforGUSTO <jamesdoth@gmail.com>
2022-07-22 17:01:05 -0700

    tailwind test card on firstpost

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,26 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const FirstPost = () => {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">First Post</h1>
-            <h2>
-                <Link href='/'>
-                    <a>Home</a>
-                </Link>
-            </h2>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>First Post</h1>
+      <h2>
+        <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
+          <div className='shrink-0'></div>
+          <div>
+            <div className='text-xl font-medium text-black'>
+              TailWindCSS Test
+            </div>
+            <Link href='/'>
+              <p className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>
+                Home
+              </p>
+            </Link>
+          </div>
+        </div>
+      </h2>
+    </>
+  );
+};
 
-export default FirstPost
-
+export default FirstPost;
