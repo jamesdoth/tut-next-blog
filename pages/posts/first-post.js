cb5d4c558e2b88a875ade8284a0c8ae97a3592fa
goforGUSTO <jamesdoth@gmail.com>
2022-07-24 19:19:11 -0700

    tinkering first post styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,26 +1,35 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+
+const YourComponent = () => (
+  <Image src='/public/profile.jpg' height={144} width={144} alt='Jimbo' />
+);
 
 const FirstPost = () => {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>First Post</h1>
-      <h2>
-        <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
-          <div className='shrink-0'></div>
-          <div>
-            <div className='text-xl font-medium text-black'>
-              TailWindCSS Test
-            </div>
-            <div className='flex'>
-              <Link href='/'>
-                <p className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>
-                  Home
-                </p>
-              </Link>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div className='max-w-lg p-4 mx-12 my-32'>
+        <h1 className='text-3xl font-bold underline'>First Post</h1>
+        <h2>
+          <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
+            <div className='shrink-0'></div>
+            <div>
+              <div className='text-xl font-medium text-black'>
+                TailWindCSS Test
+              </div>
+              <div className='flex'>
+                <Link href='/'>
+                  <p>Home</p>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </h2>
+        </h2>
+      </div>
     </>
   );
 };
