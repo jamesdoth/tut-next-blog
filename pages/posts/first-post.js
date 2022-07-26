cc47bde098b6f8d369c7c5828504d990615fe416
goforGUSTO <jamesdoth@gmail.com>
2022-07-26 10:25:05 -0700

    styled first post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -12,19 +12,18 @@ const FirstPost = () => {
       <Head>
         <title>First Post</title>
       </Head>
-      <div className='p-4 mx-12 my-64'>
-        <h1 className='text-3xl font-bold underline'>First Post</h1>
+      <div className='w-screen min-h-screen flex flex-col justify-center -my-6 items-center p-4 overflow-y-scroll'>
+        <h1 className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
+          First <a className='text-purple-300'>Post</a>
+        </h1>
         <h2>
-          <div className='p-6 max-w-sm mx-auto bg-slate-700 rounded-xl shadow-lg flex items-center space-x-4'>
-            <div className='shrink-0'></div>
+          <div className='hover:scale-105 duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6'>
             <div>
-              <div className='text-xl font-medium text-black'>
-                TailWindCSS Test
-              </div>
+              <div className='text-lg text-gray-700'>TailWindCSS Test</div>
               <div className='flex'>
                 <Link href='/'>
-                  <p className='font-medium text-blue-600 hover:text-blue-400'>
-                    Back to Home
+                  <p className='cursor-pointer text-sm text-blue-500 hover:text-blue-400'>
+                    Back to Home &larr;
                   </p>
                 </Link>
               </div>
